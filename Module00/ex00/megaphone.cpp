ed5bd248adbc8235e572099ac9335ff3de706106
Sebastiao Muller <sebastiaomuller@MacBook-Air-de-Sebastiao.local>
2023-06-10 19:34:17 +0100

    Module00 done

---
+++ Module00/ex00/megaphone.cpp
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sde-mull <sde.mull@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/10 19:25:42 by sde-mull          #+#    #+#             */
+/*   Updated: 2023/06/10 19:31:18 by sde-mull         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (1);
+	}
+	for (int i = 1; i < argc; i++)
+		for (int j = 0; argv[i][j]; j++)
+			std::cout << (char)toupper(argv[i][j]);
+	std::cout << std::endl;
+	return (0);
+}
