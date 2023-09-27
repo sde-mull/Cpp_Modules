1b3ff84dbab77fbb0423285e6d9b107874b886c4
Sebastiao Muller <sebastiaomuller@MacBook-Air-de-Sebastiao.local>
2023-09-27 15:57:02 +0100

    Module01 ready to go again

--- Module01/ex04/Sedcpp.cpp
+++ Module01/ex04/Sedcpp.cpp
@@ -6,7 +6,7 @@
 /*   By: sde-mull <sde.mull@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/14 20:11:43 by sde-mull          #+#    #+#             */
-/*   Updated: 2023/09/27 13:55:37 by sde-mull         ###   ########.fr       */
+/*   Updated: 2023/09/27 15:56:02 by sde-mull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,6 +38,7 @@ bool Sedcpp::replace(const std::string &s1, const std::string &s2)
 	std::ofstream ofs;
 	std::string to_compare;
 	size_t	position;
+	size_t	last_position;
 	
 	ifs.open(this->_infile.c_str());
 	ofs.open(this->_outfile.c_str());
@@ -49,11 +50,12 @@ bool Sedcpp::replace(const std::string &s1, const std::string &s2)
 	if (ifs.is_open()){
 		if (std::getline(ifs, to_compare, '\0')){
 			position = to_compare.find(s1);
+			last_position = position;
 			while (position != std::string::npos)
 			{
 				to_compare.erase(position, s1.length());
 				to_compare.insert(position, s2);
-				position = to_compare.find(s1, position + 1);
+				position = to_compare.find(s1, position + s2.length());
 			}
 			ofs << to_compare;
 		}
