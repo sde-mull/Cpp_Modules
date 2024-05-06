611c31b4fd1236a803036160f9acb0513487b86d
sde-mull <sde.mull.dev@gmail.com>
2024-05-06 20:21:18 +0100

    Module 07 ready to go 2

--- Module07/ex01/main.cpp
+++ Module07/ex01/main.cpp
@@ -3,29 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sde-mull <sde.mull@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: sde-mull <sde-mull@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 20:31:32 by sde-mull          #+#    #+#             */
-/*   Updated: 2024/03/08 15:14:01 by sde-mull         ###   ########.fr       */
+/*   Updated: 2024/05/06 20:17:38 by sde-mull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Iter.hpp"
 
-int main(void)
+int main( void ) 
 {
-	int intArray[] = {1, 2, 3, 4, 5};
+    int intArray[3] = { 2, 4 , 6 };
 
-    std::string strArray[] = {"Hello World"};
+    std::string strArray[4] = { "Hello", "42", "and", "World"};
 
-    void* voidArray[] = {&intArray, &strArray};
-    {
-        ::iter(static_cast<int*>(voidArray[0]), 5, &printElem);
-		std::cout << std::endl;
-    }
-    {
-        ::iter(static_cast<char*>(voidArray[1]), 11, &printElem);
-		std::cout << std::endl;
-    }
-    return (0);
+    ::iter(intArray, 3, ::printElem);
+
+    ::iter(strArray, 4, ::printElem);
+    
+    return 0;
 }
