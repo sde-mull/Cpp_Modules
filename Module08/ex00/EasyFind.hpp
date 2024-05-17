388d45ea4e3c21a4b8ea8f214400746d2698f6e4
sde-mull <sde.mull.dev@gmail.com>
2024-05-17 22:49:48 +0100

    Module08 ready to go

--- Module08/ex00/EasyFind.hpp
+++ Module08/ex00/EasyFind.hpp
@@ -6,7 +6,7 @@
 /*   By: sde-mull <sde-mull@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/06 22:02:58 by sde-mull          #+#    #+#             */
-/*   Updated: 2024/05/07 23:29:40 by sde-mull         ###   ########.fr       */
+/*   Updated: 2024/05/17 22:00:59 by sde-mull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,17 @@
 
 class IntegerNotFoundException : public std::exception
 {
-public:
-	const char *what() const throw()
-	{
-		return "";
-	}
+	public:
+		const char *what() const throw(){
+			return "Integer was not found :(";
+		}
 };
 
 template <typename T>
 void EasyFind(T &container, int number)
 {
 	typename T::iterator it = std::find(container.begin(), container.end(), number);
-	if (it == container.end())
-	{
+	if (it == container.end()){
 		std::cout << "Integer not Found: " << number << std::endl;
 		throw IntegerNotFoundException();
 	}
--- Module08/ex00/main.cpp
+++ Module08/ex00/main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: sde-mull <sde.mull@student.42lisboa.com    +#+  +:+       +#+        */
+/*   By: sde-mull <sde-mull@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/12 20:21:01 by sde-mull          #+#    #+#             */
-/*   Updated: 2024/03/09 18:06:33 by sde-mull         ###   ########.fr       */
+/*   Updated: 2024/05/17 22:13:43 by sde-mull         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,20 @@ int main(void)
 {
 	{
 		srand(time(NULL));
-		try
+		std::vector<int> container;
+		for (int i = -1; i < 20; i++)
+			container.push_back(i);
+		for (int j = 0; j < 3; j++)
 		{
-			std::vector<int> container;
-			for (int i = -1; i < 20; i++)
-				container.push_back(i);
-			for (int j = 0; j < 3; j++)
-			{
+			try{
 				const int Number = rand() % 25;
 				::EasyFind(container, Number);
 			}
+			catch (std::exception &exc){
+				std::cout << exc.what() << std::endl;
+				}
 		}
-		catch (std::exception &exc)
-		{
-			std::cout << exc.what() << std::endl;
-		}
+		
 	}
 	return (0);
 }
